7aa316fc184448a8d1efe49affc2cfa727c26932
Addi3 <addiec787@gmail.com>
2024-10-02 20:55:53 +0100

    gallifrey done (for now, waiting for citadel)

--- src/main/java/net/addie/aitplus/init/AitplusModProcedures.java
+++ src/main/java/net/addie/aitplus/init/AitplusModProcedures.java
@@ -6,11 +6,15 @@ package net.addie.aitplus.init;
 
 import net.addie.aitplus.procedures.TakeMeToTrenzalorRightClickedProcedure;
 import net.addie.aitplus.procedures.TakeMeToGallifreyRightclickedProcedure;
+import net.addie.aitplus.procedures.CadonwoodOnBlockRightClickedProcedure;
+import net.addie.aitplus.procedures.CadonLogOnBlockRightClickedProcedure;
 
 @SuppressWarnings("InstantiationOfUtilityClass")
 public class AitplusModProcedures {
 	public static void load() {
 		new TakeMeToGallifreyRightclickedProcedure();
 		new TakeMeToTrenzalorRightClickedProcedure();
+		new CadonwoodOnBlockRightClickedProcedure();
+		new CadonLogOnBlockRightClickedProcedure();
 	}
 }
