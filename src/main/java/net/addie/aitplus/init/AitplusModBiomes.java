858ec87f6ff7898b837e4adaca415b4bf188ce78
Addi3 <addiec787@gmail.com>
2024-10-02 23:15:12 +0100

    trenzalore tardis structure

--- src/main/java/net/addie/aitplus/init/AitplusModBiomes.java
+++ src/main/java/net/addie/aitplus/init/AitplusModBiomes.java
@@ -4,22 +4,40 @@
  */
 package net.addie.aitplus.init;
 
+import net.minecraft.world.level.levelgen.SurfaceRules;
+import net.minecraft.world.level.levelgen.NoiseGeneratorSettings;
+import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import net.minecraft.world.level.dimension.LevelStem;
 import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.dimension.BuiltinDimensionTypes;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.biome.MultiNoiseBiomeSource;
+import net.minecraft.world.level.biome.FeatureSorter;
+import net.minecraft.world.level.biome.Climate;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.Registry;
+import net.minecraft.core.Holder;
 
+import net.addie.aitplus.mixins.NoiseGeneratorSettingsAccess;
 import net.addie.aitplus.AitplusMod;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import com.mojang.datafixers.util.Pair;
+
+import com.google.common.base.Suppliers;
+
 public class AitplusModBiomes {
-	public static ResourceKey<Biome> TRENZALORE_PLAINS = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "trenzalore_plains"));
 	public static ResourceKey<Biome> GALLIFREY_DRYLANDS = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "gallifrey_drylands"));
 	public static ResourceKey<Biome> GALLIFREY_PLAINS = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "gallifrey_plains"));
 	public static ResourceKey<Biome> GALLIFREY_MOUNTAINS = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "gallifrey_mountains"));
+	public static ResourceKey<Biome> GALLIFREY_DEATH_ZONE = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "gallifrey_death_zone"));
+	public static ResourceKey<Biome> TRENZALORE_WASTES = ResourceKey.create(Registries.BIOME, new ResourceLocation(AitplusMod.MODID, "trenzalore_wastes"));
 
 	public static void loadEndBiomes() {
 	}
@@ -30,6 +48,30 @@ public class AitplusModBiomes {
 		Registry<Biome> biomeRegistry = server.registryAccess().registryOrThrow(Registries.BIOME);
 		for (LevelStem levelStem : levelStemTypeRegistry.stream().toList()) {
 			DimensionType dimensionType = levelStem.type().value();
+			if (dimensionType == dimensionTypeRegistry.getOrThrow(BuiltinDimensionTypes.OVERWORLD)) {
+				ChunkGenerator chunkGenerator = levelStem.generator();
+				// Inject biomes to biome source
+				if (chunkGenerator.getBiomeSource() instanceof MultiNoiseBiomeSource noiseSource) {
+					List<Pair<Climate.ParameterPoint, Holder<Biome>>> parameters = new ArrayList<>(noiseSource.parameters().values());
+					parameters.add(new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(0.3f, 1f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.point(0.0f),
+							Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, new ResourceLocation("aitplus", "gallifrey_death_zone")))));
+					parameters.add(new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(0.3f, 1f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.point(1.0f),
+							Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, new ResourceLocation("aitplus", "gallifrey_death_zone")))));
+					parameters.add(new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(0.3f, 1f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.point(0.0f),
+							Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, new ResourceLocation("aitplus", "trenzalore_wastes")))));
+					parameters.add(new Pair<>(new Climate.ParameterPoint(Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.span(0.3f, 1f), Climate.Parameter.span(-0.5f, 0.5f), Climate.Parameter.point(1.0f),
+							Climate.Parameter.span(-1f, 1f), 0), biomeRegistry.getHolderOrThrow(ResourceKey.create(Registries.BIOME, new ResourceLocation("aitplus", "trenzalore_wastes")))));
+					chunkGenerator.biomeSource = MultiNoiseBiomeSource.createFromList(new Climate.ParameterList<>(parameters));
+					chunkGenerator.featuresPerStep = Suppliers
+							.memoize(() -> FeatureSorter.buildFeaturesPerStep(List.copyOf(chunkGenerator.biomeSource.possibleBiomes()), biome -> chunkGenerator.generationSettingsGetter.apply(biome).features(), true));
+				}
+
+				// Inject surface rules
+				if (chunkGenerator instanceof NoiseBasedChunkGenerator noiseGenerator) {
+					NoiseGeneratorSettings noiseGeneratorSettings = noiseGenerator.settings.value();
+					((NoiseGeneratorSettingsAccess) (Object) noiseGeneratorSettings).addSurfaceRules(SurfaceRules.sequence(AitplusModSurfaceRules.makeOverworldRules(), noiseGeneratorSettings.surfaceRule()));
+				}
+			}
 
 		}
 	}
--- src/main/java/net/addie/aitplus/init/AitplusModSurfaceRules.java
+++ src/main/java/net/addie/aitplus/init/AitplusModSurfaceRules.java
@@ -12,14 +12,29 @@
 */
 package net.addie.aitplus.init;
 
+import net.minecraft.world.level.levelgen.placement.CaveSurface;
 import net.minecraft.world.level.levelgen.SurfaceRules;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.resources.ResourceKey;
 
 public class AitplusModSurfaceRules {
 	public static SurfaceRules.RuleSource makeOverworldRules() {
-		return SurfaceRules.sequence();
+		return SurfaceRules.sequence(preliminarySurfaceRule(AitplusModBiomes.GALLIFREY_DEATH_ZONE, Blocks.GRASS_BLOCK.defaultBlockState(), Blocks.DIRT.defaultBlockState(), Blocks.DIRT.defaultBlockState()),
+				preliminarySurfaceRule(AitplusModBiomes.TRENZALORE_WASTES, AitplusModBlocks.TRENZALORIAN_STONE.defaultBlockState(), Blocks.BLACKSTONE.defaultBlockState(), Blocks.BLACKSTONE.defaultBlockState()));
 	}
 
 	public static SurfaceRules.RuleSource makeNetherRules() {
 		return SurfaceRules.sequence();
+	}
+
+	private static SurfaceRules.RuleSource preliminarySurfaceRule(ResourceKey<Biome> biomeKey, BlockState groundBlock, BlockState undergroundBlock, BlockState underwaterBlock) {
+		return SurfaceRules.ifTrue(SurfaceRules.isBiome(biomeKey),
+				SurfaceRules.ifTrue(SurfaceRules.abovePreliminarySurface(),
+						SurfaceRules.sequence(
+								SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(0, false, 0, CaveSurface.FLOOR),
+										SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.waterBlockCheck(-1, 0), SurfaceRules.state(groundBlock)), SurfaceRules.state(underwaterBlock))),
+								SurfaceRules.ifTrue(SurfaceRules.stoneDepthCheck(0, true, 0, CaveSurface.FLOOR), SurfaceRules.state(undergroundBlock)))));
 	}
 }
