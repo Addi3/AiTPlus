156f4ae4332b147ff4c6797cf52b5c3e776f3481
Addi3 <addiec787@gmail.com>
2024-10-12 21:00:51 +0100

    fixed jukebox top z-fighting and changed emissive texture

--- src/main/java/net/addie/aitplus/block/JukeboxTopBlock.java
+++ src/main/java/net/addie/aitplus/block/JukeboxTopBlock.java
@@ -42,8 +42,13 @@ public class JukeboxTopBlock extends Block {
 	}
 
 	@Override
+	public boolean propagatesSkylightDown(BlockState state, BlockGetter reader, BlockPos pos) {
+		return true;
+	}
+
+	@Override
 	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
-		return 3;
+		return 0;
 	}
 
 	@Override
